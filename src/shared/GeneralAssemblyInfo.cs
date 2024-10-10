e5876b1f3f984775bb7d133166e2d8fbaeef7ea6
RzR <108324929+I-RzR-I@users.noreply.github.com>
2024-10-10 15:11:30 +0300

    Update reference package version, fixing CVE

--- src/shared/GeneralAssemblyInfo.cs
+++ src/shared/GeneralAssemblyInfo.cs
@@ -43,6 +43,6 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
-[assembly: AssemblyVersion("1.0.4.7652")]
-[assembly: AssemblyFileVersion("1.0.4.7652")]
-[assembly: AssemblyInformationalVersion("1.0.4.7652")]
+[assembly: AssemblyVersion("1.0.5.5444")]
+[assembly: AssemblyFileVersion("1.0.5.5444")]
+[assembly: AssemblyInformationalVersion("1.0.5.5444")]
