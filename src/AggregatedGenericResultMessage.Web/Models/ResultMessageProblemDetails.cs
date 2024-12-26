0f73d3d93af66fac1a8457aed490047a5d25181e
RzR <108324929+I-RzR-I@users.noreply.github.com>
2024-12-26 20:54:55 +0200

    Add new ProblemDetails result model.
    Add new ProblemDetails result model. Extend existing Microsoft.AspNetCore.Mvc.ProblemDetails

---
+++ src/AggregatedGenericResultMessage.Web/Models/ResultMessageProblemDetails.cs
@@ -0,0 +1,47 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.ResultMessage.AggregatedGenericResultMessage.Web
+//  Author           : RzR
+//  Created On       : 2024-12-24 22:03
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2024-12-25 13:56
+// ***********************************************************************
+//  <copyright file="ResultMessageProblemDetails.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+// ReSharper disable CollectionNeverQueried.Global
+
+#endregion
+
+namespace AggregatedGenericResultMessage.Web.Models
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     Information about the result message problem.
+    /// </summary>
+    /// <seealso cref="T:Microsoft.AspNetCore.Mvc.ProblemDetails"/>
+    /// =================================================================================================
+    public class ResultMessageProblemDetails : ProblemDetails
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Gets the extensions.
+        /// </summary>
+        /// <value>
+        ///     The extensions.
+        /// </value>
+        /// =================================================================================================
+        public IDictionary<string, object> Extensions { get; } = new Dictionary<string, object>(StringComparer.Ordinal);
+    }
+}
