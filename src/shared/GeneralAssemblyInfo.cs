75bcde9509f042d1f06ff05e17312fbc9cce5a92
RzR <108324929+I-RzR-I@users.noreply.github.com>
2026-09-10 21:29:08 +0300

    Auto commit uncommited files

--- src/shared/GeneralAssemblyInfo.cs
+++ src/shared/GeneralAssemblyInfo.cs
@@ -43,6 +43,6 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
-[assembly: AssemblyVersion("3.1.0.7713")]
-[assembly: AssemblyFileVersion("3.1.0.7713")]
-[assembly: AssemblyInformationalVersion("3.1.0.7713")]
+[assembly: AssemblyVersion("4.0.0.7735")]
+[assembly: AssemblyFileVersion("4.0.0.7735")]
+[assembly: AssemblyInformationalVersion("4.0.0.7735")]
